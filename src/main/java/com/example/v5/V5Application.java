121d03cdd2ade6de91c7080d5192d9662c537299
Pradeep <er.spider29@gmail.com>
2018-06-21 01:44:30 +0530

    base

---
+++ src/main/java/com/example/v5/V5Application.java
@@ -0,0 +1,12 @@
+package com.example.v5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(V5Application.class, args);
+    }
+}
